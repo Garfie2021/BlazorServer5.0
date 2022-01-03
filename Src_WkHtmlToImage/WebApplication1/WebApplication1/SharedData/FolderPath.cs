68c68d38b55d43921aea0bc43e655d642ba8855f
キャベツウニ <akihitoukami@gmail.com>
2022-01-03 18:33:56 +0900

    1

---
+++ Src_WkHtmlToImage/WebApplication1/WebApplication1/SharedData/FolderPath.cs
@@ -0,0 +1,15 @@
+﻿
+namespace WebApplication1.SharedData
+{
+    /// <summary>
+    /// アプリケーション全体で共有するパス情報などを纏めるクラス
+    /// </summary>
+    public static class FolderPath
+    {
+        /// <summary>
+        /// ダウンロードファイル置き場
+        /// </summary>
+        public static string Download;
+
+    }
+}
