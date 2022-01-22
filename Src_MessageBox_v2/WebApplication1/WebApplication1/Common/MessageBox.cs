df3ddb402f26b2160d9c01cadbe48fb1dc7e786e
キャベツウニ <akihitoukami@gmail.com>
2022-01-22 09:41:24 +0900

    1

---
+++ Src_MessageBox_v2/WebApplication1/WebApplication1/Common/MessageBox.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Components;
+using Blazored.Modal;
+using Blazored.Modal.Services;
+using WebApplication1.Modal;
+
+
+namespace WebApplication1
+{
+    /// <summary>
+    /// Windows Form の MessageBoxリスペクト クラス
+    /// </summary>
+    public class MessageBox
+    {
+        private IModalService Modal;
+
+        public MessageBox(IModalService modal)
+        {
+            Modal = modal;
+        }
+
+        /// <summary>
+        /// 確認画面表示
+        /// </summary>
+        public async Task<ModalResult> Show(string caption, string text, MessageBoxButtons buttons, MessageBoxIcon icon)
+        {
+            var parameters = new ModalParameters();
+            parameters.Add(nameof(ModalMessageBox.Message), text);
+            parameters.Add(nameof(ModalMessageBox.MessageDetail), null);
+            parameters.Add(nameof(ModalMessageBox.Buttons), buttons);
+            parameters.Add(nameof(ModalMessageBox.Icon), icon);
+
+            var moviesModal = Modal.Show<ModalMessageBox>(caption, parameters);
+            var result = await moviesModal.Result;
+            await Task.Delay(1);//これがないとメッセージボックスが閉じられるまでのタイムラグが発生することがある。
+
+            return result;
+        }
+
+        public async Task<ModalResult> Show(string caption, string text, string textDetail, MessageBoxButtons buttons, MessageBoxIcon icon)
+        {
+            var parameters = new ModalParameters();
+            parameters.Add(nameof(ModalMessageBox.Message), text);
+            parameters.Add(nameof(ModalMessageBox.MessageDetail), textDetail);
+            parameters.Add(nameof(ModalMessageBox.Buttons), buttons);
+            parameters.Add(nameof(ModalMessageBox.Icon), icon);
+
+            var moviesModal = Modal.Show<ModalMessageBox>(caption, parameters);
+            var result = await moviesModal.Result;
+            await Task.Delay(1);//これがないとメッセージボックスが閉じられるまでのタイムラグが発生することがある。
+
+            return result;
+        }
+
+    }
+}
---
+++ Src_MessageBox_v2/WebApplication1/WebApplication1/Const/enums/MessageBoxButtons.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    // Windows Form アプリからコピーしたenum。
+    //
+    // 概要:
+    //     表示するボタンを定義する定数を指定する System.Windows.Forms.MessageBoxです。
+    public enum MessageBoxButtons
+    {
+        //
+        // 概要:
+        //     メッセージ ボックスには、[ok] ボタンが含まれています。
+        OK = 0,
+        //
+        // 概要:
+        //     メッセージ ボックスには、[ok] と [キャンセル] ボタンが含まれています。
+        OKCancel = 1,
+        //
+        // 概要:
+        //     メッセージ ボックスには、[はい] と [いいえ] ボタンが含まれています。
+        YesNo = 4,
+    }
+
+}
