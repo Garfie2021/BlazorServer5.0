5cb50b2b8739b9ed38425b9adbb6083df989d165
キャベツウニ <akihitoukami@gmail.com>
2022-02-06 11:08:19 +0900

    1

---
+++ Src_Disabled/WebApplication1/WebApplication1/Util/Extension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Util
+{
+    public static class Extension
+    {
+
+        /// <summary>
+        /// Htmlタグ disabled を、簡潔に記述する為の拡張メソッド。
+        /// </summary>
+        public static string ToDisabled(this bool src)
+        {
+            return !src ? "disabled" : null;
+        }
+
+    }
+}
