3a3ed21f54783847e88bfd13a37ff699d932503f
キャベツウニ <akihitoukami@gmail.com>
2022-02-17 19:21:57 +0900

    1

---
+++ Src_PageBase/WebApplication1/WebApplication1/Data/TestCsv.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CsvHelper.Configuration.Attributes;
+
+namespace WebApplication1.Data
+{
+    public class TestCsv
+    {
+        [Name("テスト列１")]
+        public string TestClm1 { get; set; }
+
+        [Name("テスト列２")]
+        public string TestClm2 { get; set; }
+
+        [Name("テスト列３")]
+        public string TestClm3 { get; set; }
+    }
+}
