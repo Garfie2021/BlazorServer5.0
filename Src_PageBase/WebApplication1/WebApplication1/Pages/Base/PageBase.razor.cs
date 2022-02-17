42459e26dc02f7449bf1b8ff9f485c41b85a6c84
キャベツウニ <akihitoukami@gmail.com>
2022-02-17 23:45:25 +0900

    1

---
+++ Src_PageBase/WebApplication1/WebApplication1/Pages/Base/PageBase.razor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using BlazorDownloadFile;
+
+namespace WebApplication1.Pages.Base
+{
+    partial class PageBase
+    {
+        [Inject] protected IBlazorDownloadFileService BlazorDownloadFileService { get; set; }
+
+        [Inject] protected NavigationManager _NavigationManager { get; set; }
+        [Inject] protected IJSRuntime JsRuntime { get; set; }
+    }
+}
+
+
