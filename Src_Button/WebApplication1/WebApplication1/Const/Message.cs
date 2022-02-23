207ea610f0dc29954e026e41a1c9803ee31b2f98
キャベツウニ <akihitoukami@gmail.com>
2022-02-23 14:35:44 +0900

    1

---
+++ Src_Button/WebApplication1/WebApplication1/Const/Message.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Const
+{
+    public static class Message
+    {
+        public const string RequiredErrorMessage = "{0}は必須です。";
+    }
+}
---
+++ Src_Button/WebApplication1/WebApplication1/Model/DataModel1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Const;
+
+namespace WebApplication1.Model
+{
+    public class DataModel1
+    {
+        [Display(Name = "入力欄A")]
+        [Required(ErrorMessage = Message.RequiredErrorMessage)]
+        public string InputA { get; set; }
+    }
+}
