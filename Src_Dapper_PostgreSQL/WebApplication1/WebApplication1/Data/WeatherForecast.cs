cf6c349727660acf78e471778ec1cc0c159a9b6c
ukami <ukami@interserve.co.jp>
2022-05-03 16:07:56 +0900

    1

--- Src_Dapper_PostgreSQL/WebApplication1/WebApplication1/Data/WeatherForecast.cs
+++ Src_Dapper_PostgreSQL/WebApplication1/WebApplication1/Data/WeatherForecast.cs
@@ -1,6 +1,4 @@
-using System;
-
-namespace WebApplication1.Data
+namespace BlazorApp1.Data
 {
     public class WeatherForecast
     {
@@ -10,6 +8,6 @@ namespace WebApplication1.Data
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string Summary { get; set; }
+        public string? Summary { get; set; }
     }
 }
